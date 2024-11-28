953322d72c70c62b382f68b15f8c3ee7933c8765
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2024-11-28 13:25:03 +0300

    SafeAreaView

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,22 @@
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 function GameScreen() {
-  return <Text>Game Screen!</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's gUESS</Text>
+      <View>
+        <Text>Higher or Lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
